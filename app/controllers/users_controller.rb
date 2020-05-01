1ab4fa39de0d24fa9e81b9de3bd39da44c8e2220
Yessica Delgado <yessica@cooltiva.mx>
2020-04-30 21:58:21 -0500

    add the gem file, work on venues controller for new and create, create the form and new page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  def new
+    @venue = Venue.new # needed to instantiate the form_for
+  end
+
+   def create
+    @venue = Venue.new(venue_params)
+    if @venue.save
+      redirect_to venue_path(@venue)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :address, :description, :price, :picture_url)
+  end
 end
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,20 @@
 class VenuesController < ApplicationController
+  def new
+    @venue = Venue.new
+  end
+
+   def create
+    @venue = Venue.new(venue_params)
+    if @venue.save
+      redirect_to venue_path(@venue)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :address, :description)
+  end
 end
