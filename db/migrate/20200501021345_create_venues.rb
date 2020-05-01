d19299db0211c1e1345bdc0d4b3fce8cfa43eb68
Lilia Jerbi <liliajer@buffalo.edu>
2020-04-30 21:15:16 -0500

    generated venues model and controller

---
+++ db/migrate/20200501021345_create_venues.rb
@@ -0,0 +1,12 @@
+class CreateVenues < ActiveRecord::Migration[6.0]
+  def change
+    create_table :venues do |t|
+      t.string :name
+      t.text :description
+      t.string :address
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
